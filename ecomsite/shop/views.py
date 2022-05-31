48c132b203ff4b0b5d796b4ccbbbe3b19aeea815
Sanjeevani <sanjeevani.kashyap@gmail.com>
2022-05-31 23:22:18 +0530

    first commit

---
+++ ecomsite/shop/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from .models import Products, Order
+from django.core.paginator import Paginator
+
+# Create your views here.
+
+def index(request):
+    product_objects = Products.objects.all()
+
+    product_name=request.GET.get('product_name')
+
+    #search code
+    if product_name != '' and product_name is not None:
+        product_objects = product_objects.filter(title__icontains=product_name)
+
+    #paginator code
+    paginator = Paginator(product_objects,3)
+    page = request.GET.get('page')
+    product_objects = paginator.get_page(page)
+
+    return render(request,'shop/index.html',{'product_objects':product_objects})
+
+def detail(request,id):
+    product_object = Products.objects.get(id=id)
+
+    return render(request,'shop/detail.html',{'product_object':product_object})
+
+def checkout(request):
+    if request.method == "POST":
+        items = request.POST.get('items',"")
+        name = request.POST.get('name',"")
+        email = request.POST.get('email',"")
+        address = request.POST.get('address',"")
+        city = request.POST.get('city',"")
+        state = request.POST.get('state',"")
+        zipcode = request.POST.get('zipcode',"")
+        total = request.POST.get('total',"")
+
+        order = Order(items=items,name=name,email=email,address=address,city=city,state=state,zipcode=zipcode,total=total)  
+        order.save()  
+
+    return render(request,'shop/checkout.html')
